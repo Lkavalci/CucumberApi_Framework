a613501221c1f32974f80a14758fc20270a7665a
Lkavalci <lutfikavalci@gmail.com>
2023-06-27 00:10:32 +0300

    230627END

--- src/test/java/hooks/api/HooksAPI.java
+++ src/test/java/hooks/api/HooksAPI.java
@@ -1,19 +1,30 @@
 package hooks.api;
 
+import io.cucumber.java.Before;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
-import org.junit.Before;
+
+import utilities.Authentication;
 import utilities.ConfigReader;
+
 
 public class HooksAPI {
 
-    RequestSpecification spec;
+    public static RequestSpecification spec;
+    public static String token;
 
-    @Before
+    @Before (order=0)
     public void setUp(){
 
-        spec=new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("base_Url")).build();
-
+        spec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("base_url")).build();
 
     }
+
+    @Before (order=1)
+    public void beforeGenerateToken(){
+
+        token = Authentication.generateToken();
+
+    }
+
 }
