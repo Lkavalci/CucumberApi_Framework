36d81acada55a992b4fe7511b3913592b8d2ab5b
Lkavalci <lutfikavalci@gmail.com>
2023-07-09 00:04:05 +0300

    a

--- src/test/java/fayk.java
+++
@@ -1,5 +0,0 @@
-public class fayk {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/utilities/cayk_fake.java
@@ -0,0 +1,7 @@
+package utilities;
+
+public class cayk_fake {
+    public static void main(String[] args) {
+        System.out.println("github deneme");
+    }
+}
